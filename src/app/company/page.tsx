7670ffd0448ea8d9f33dfa9da288a308e8e17d1d
CoderGhost37 <coderghost37@gmail.com>
2024-10-21 23:41:49 +0530

    feat: company page

---
+++ src/app/company/page.tsx
@@ -0,0 +1,13 @@
+export function CompanyPage() {
+  return (
+    <div className='py-12'>
+      <iframe
+        allowFullScreen
+        src='https://designrr.page/?id=385327&token=2265566852&type=FP&h=6830'
+        height='600'
+        width='100%'
+        frameBorder='0'
+      />
+    </div>
+  );
+}
